f268e25c5a146f6cd41c2c29816ce3abfd0bf040
Daniel Boscan <danielboscanramos@gmail.com>
2022-11-24 14:35:45 -0300

    finish

--- js/app.js
+++ js/app.js
@@ -52,19 +52,19 @@ function closeBodyToggle() {
     div.style.display = 'none';
 }
 
-// window.addEventListener("scroll", (event) => {
-//     let scrollY = this.scrollY;
-//     let scrollX = this.scrollX;
-//     if(scrollX != 0 || scrollY != 0){
-//         let div = document.getElementById('home');
-//         div.style.position = 'absolute';
-//     } else if(scrollX == 0 || scrollY == 0) {
-//         let div = document.getElementById('home');
-//         div.style.position = 'fixed';
-//         div.style.top = '0';
-//         div.style.right = '0';
-//         div.style.left = '0';
-//         div.style.left = '0';
-//         div.style.zIndex = '1030';
-//     }
-// });
+$(document).ready(function() {
+    $('.top').click(function() {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.top').slideDown(300);
+        } else {
+            $('.top').slideUp(300);
+        }
+    });
+
+});
